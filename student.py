2f330b9201d834c4eec893905945cb3a93885ec4
kirchnerk <kirchnerk@gilmour.org>
2016-10-05 08:56:06 -0400

    Started student dance

--- student.py
+++ student.py
@@ -47,6 +47,17 @@ class GoPiggy(pigo.Pigo):
         print("Piggy dance")
         ##### WRITE YOUR FIRST PROJECT HERE
         self.encR(18)
+        for x in range(100,200, 25):
+            print('Speed is set to: ' + str(x))
+            servo(20)
+            set_speed(x)
+            self.encB(5)
+            self.encR(2)
+            self.encL(2)
+            self.encF(5)
+            servo(120)
+            time.sleep(.1)
+
     # AUTONOMOUS DRIVING
     def nav(self):
         print("Piggy nav")
