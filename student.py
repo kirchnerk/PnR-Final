f0e82a681fb6f3948e7f6bb511b67e4b36a75901
kirchnerk <kirchnerk@gilmour.org>
2016-12-05 09:46:26 -0500

    Do now edits

--- student.py
+++ student.py
@@ -175,61 +175,68 @@ class GoPiggy(pigo.Pigo):
 
     #Our new code to make the robot go forward and find openings to not just rely on previous turns
     #this method is my scanning method to make sure my robot doesn't smash into things or get stuck
+    #################################
+    ### THE KENNY METHOD OF SCANNING - experimental
+
     def kenny(self):
-        # erase anything saved in the scan array
+        #Activate our scanner!
         self.wideScan()
-        #count will keep track of contigeous postive readings
-        #Tryig to speed up the scan
-        for x in range(self.MIDPOINT-60, self.MIDPOINT+60, 2):
-            servo(x)
-            time.sleep(.1)
-            self.scan[x] = us_dist(15)
-            time.sleep(.05)
-
-        # count will be used to find 20 degrees of openings
+        # count will keep track of contigeous positive readings
         count = 0
-        #list of all the open paths we detect
-        # this list will keep track of windows of free space
+        # list of all the open paths we detect
         option = [0]
+        #YOU DECIDE: What do we add to STOP_DIST when looking for a path fwd?
+        SAFETY_BUFFER = 30
+        #YOU DECIDE: what increment do you have your wideScan set to?
         INC = 2
-        SAFTEY_BUFFER = 30
 
-        ###################
-        #BUILD THE OPTIONS#
-        ###################
+        ###########################
+        ######### BUILD THE OPTIONS
+        #loop from the 60 deg right of our middle to 60 deg left of our middle
+        for x in range(self.MIDPOINT - 60, self.MIDPOINT + 60):
+            # ignore all blank spots in the list
+            if self.scan[x]:
+                # add 30 if you want, this is an extra safety buffer
+                if self.scan[x] > (self.STOP_DIST + SAFETY_BUFFER):
+                    count += 1
+                # if this reading isn't safe...
+                else:
+                    # aww nuts, I have to reset the count, this path won't work
+                    count = 0
+                #YOU DECIDE: Is 16 degrees the right size to consider as a safe window?
+                if count > (16 / INC) - 1:
+                    # SUCCESS! I've found enough positive readings in a row
+                    print("---FOUND OPTION: from " + str(x - 16) + " to " + str(x))
+                    #set the counter up again for next time
+                    count = 0
+                    #add this option to the list
+                    option.append(x - 8)
 
-        for x in range(self.MIDPOINT-60, self.MIDPOINT+60, 2):
-            if scan[x]:
-            # if the distance is far enough away, count this spot
-            #adding 30 is a saftey buffer
-            if self.scan[x] > (self.STOP_DIST + 30):
-                count += 1
-            # if there was a spot that wasn't safe, reset counter
-            else:
-                #this resets the count if the path doesn't work
-                count=0
-            #if we've found 10 in a row, let's bookmark the spot
-            if count==(20/INC):
-                print("Found an option from "+ str(x-20)+" to "+ str(x)+ " degrees")
-                count = 0
-                option.append(x-10)
-                self.dataBase()
+        ####################################
+        ############## PICK FROM THE OPTIONS - experimental
 
-        #######################
-        #PICK FROM THE OPTIONS#
-        #######################
+        #The biggest angle away from our midpoint we could possibly see is 90
         bestoption = 90
-        winner = 0
+        #the turn it would take to get us aimed back toward the exit - experimental
+        ideal = -self.turn_track
+        print("\nTHINKING. Ideal turn: " + str(ideal) + " degrees\n")
+        # x will iterate through all the angles of our path options
         for x in option:
-            #skip our filler option
-            if not x.__index__() == 0:
-                print("Choice #" + str(x.__index__()) +"is@" + str(x) + "degrees")
-                ideal = self.turn_track +self.MIDPOINT
-                print("My ideal choice would be " + str(ideal))
-            if bestoption > abs(self.turn_track -(x - self.MIDPOINT))
-                bestoption = abs(self.turn_track -(x - self.MIDPOINT))
-                winner = x - self.MIDPOINT
-        return winner
+            # skip our filler option
+            if x != 0:
+                # the change to the midpoint needed to aim at this path
+                turn = self.MIDPOINT - x
+                # state our logic so debugging is easier
+                print("\nPATH @  " + str(x) + " degrees means a turn of " + str(turn))
+                # if this option is closer to our ideal than our current best option...
+                if abs(ideal - bestoption) > abs(ideal - turn):
+                    # store this turn as the best option
+                    bestoption = turn
+        if bestoption > 0:
+            input("\nABOUT TO TURN RIGHT BY: " + str(bestoption) + " degrees")
+        else:
+            input("\nABOUT TO TURN LEFT BY: " + str(abs(bestoption)) + " degrees")
+        return bestoption
 
 
     #Ben and I shared the code Mr. A (you) helped create, copied from Ben's code is the selecting path part
